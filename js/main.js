bfef490ca67364dee3b9bd9ac713a3e8cca5c3e7
Nicholas Nelson <nnelson263@gmail.com>
2016-11-04 12:02:39 +1030

    Add color to the square

--- js/main.js
+++ js/main.js
@@ -61,6 +61,9 @@ function initShaders() {
 	vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
 	gl.enableVertexAttribArray(vertexPositionAttribute);
 
+	vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+	gl.enableVertexAttribArray(vertexColorAttribute);
+
 }
 
 function getShader(gl, id, type) {
@@ -114,6 +117,17 @@ function initBuffers() {
 	];
 
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+	var colors = [
+	    1.0,  1.0,  1.0,  1.0,    // white
+	    1.0,  0.0,  0.0,  1.0,    // red
+	    0.0,  1.0,  0.0,  1.0,    // green
+	    0.0,  0.0,  1.0,  1.0     // blue
+	];
+	  
+	squareVerticesColorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
 }
 
 function drawScene() {
@@ -126,6 +140,8 @@ function drawScene() {
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesBuffer);
 	gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, squareVerticesColorBuffer);
+	gl.vertexAttribPointer(vertexColorAttribute, 4, gl.FLOAT, false, 0, 0);
 	setMatrixUniforms();
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 }
